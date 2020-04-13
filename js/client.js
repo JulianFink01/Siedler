1fa485f27fea685979639dba22666a688ee9a2df
Julian Fink <julianfink01@gmail.com>
2020-04-13 17:50:32 +0200

    ordnung

--- js/client.js
+++ js/client.js
@@ -2,12 +2,12 @@ var socket;
   socket = io.connect('http://localhost:3000');
 
   socket.on('info',
-    function(text) {
-      document.getElementById('echotext').innerHTML = text;
-    }
+    get(text)
   );
 
-
+function get(text){
+    document.getElementById('echotext').innerHTML = text;
+}
 function senden(){
   var text =   document.getElementById('text').value;
   socket.emit('info',text);
