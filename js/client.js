0d686b8948ccec5af0139d09070a30ea5977e75d
Julian Fink <julianfink01@gmail.com>
2020-04-13 19:02:30 +0200

    fix

--- js/client.js
+++ js/client.js
@@ -1,19 +1,15 @@
-
-  //Socket wird erstellt
   var socket;
   socket = io.connect('http://localhost:3000');
-  //Wenn eine Msg mit den Namen "info" hereinkommt führt der socket folgende Funktion aus: get()
+
   socket.on('info',
-    get(text)
+    function(text){
+      document.getElementById('echotext').innerHTML = text;
+    }
   );
 
-function get(text){
-    document.getElementById('echotext').innerHTML = text;
-}
 
-//Socket sendet Information
+
 function senden(){
   var text =   document.getElementById('text').value;
   socket.emit('info',text);
-  document.getElementById('echotext').innerHTML = text;
 }
