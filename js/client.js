2c3f57f902852c72b10855f686c9e6a06af3da7b
Julian Fink <julianfink01@gmail.com>
2020-04-13 17:54:59 +0200

    Erklärungen

--- js/client.js
+++ js/client.js
@@ -1,6 +1,8 @@
-var socket;
-  socket = io.connect('http://localhost:3000');
 
+  //Socket wird erstellt
+  var socket;
+  socket = io.connect('http://localhost:3000');
+  //Wenn eine Msg mit den Namen "info" hereinkommt führt der socket folgende Funktion aus: get()
   socket.on('info',
     get(text)
   );
@@ -8,6 +10,8 @@ var socket;
 function get(text){
     document.getElementById('echotext').innerHTML = text;
 }
+
+//Socket sendet Information
 function senden(){
   var text =   document.getElementById('text').value;
   socket.emit('info',text);
--- js/server.js
+++ js/server.js
@@ -1,32 +1,32 @@
-
+//Libary wird geladen
 var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 3000, listen);
 
+//Server schaut ob verbindungen existieren
 function listen() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+  console.log('listening at http://' + host + ':' + port);
 }
-
+//Socket wird erstellt
 app.use(express.static('public'));
-
 var io = require('socket.io')(server);
-
-
+//Socket wird auf connection erstellt
 io.sockets.on('connection',
+  //Wenn neue Verbindung wird hier gelistet
   function (socket) {
-
-    console.log("We have a new client: " + socket.id);
+    console.log("New Client: " + socket.id);
+    //Bei Socket msg mit den Namen "info" reagiert der Socket und führt die folgende Funktion aus
     socket.on('info',
       function(text) {
         console.log("Received: 'text' " + text);
-
+        //Sockete wird an alle Teilnhemer weitergeleitet
         socket.broadcast.emit('info', text);
-
       }
     );
 
+    //Bei disconnection
     socket.on('disconnect', function() {
       console.log("Client has disconnected");
     });
